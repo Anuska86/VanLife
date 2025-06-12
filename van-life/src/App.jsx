1b09b9dc28f974fbf5a820217ea95dfd6cb06f2a
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-06-12 15:23:48 +0200

    style

--- van-life/src/App.jsx
+++ van-life/src/App.jsx
@@ -3,6 +3,7 @@ import "./index.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import Vans from "./components/pages/Vans";
 
 export default function App() {
   return (
@@ -13,12 +14,14 @@ export default function App() {
         </Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ van-life/src/components/pages/Vans.jsx
@@ -0,0 +1,10 @@
+import React, { useDeferredValue } from "react";
+import { Link } from "react-router-dom";
+
+export default function Vans() {
+  return (
+    <div className="vans-page-container">
+      <h1>Our Vans</h1>
+    </div>
+  );
+}
