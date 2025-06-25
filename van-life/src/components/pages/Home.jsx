d09c2f55a7326bc3a1da197869c201301313b890
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-06-25 15:44:52 +0200

    style

--- van-life/src/components/pages/Home.jsx
+++ van-life/src/components/pages/Home.jsx
@@ -5,14 +5,22 @@ import Scottland from "..//../images/Scottland.png";
 export default function Home() {
   return (
     <div className="home-container">
-      <h1>You got the travel plans, we got the travel vans.</h1>
-      <p>
-        Add adventure to your life by joining the #vanlife movement. Rent the
-        perfect van to make your perfect road trip.
-      </p>
-      <img src={Scottland} className="scottland-image" />
+      <img
+        src={Scottland}
+        className="scottland-image"
+        alt="Scottland"
+        aria-hidden="true"
+      />
+      <div className="scottland-overlay"></div>
+      <div className="home-content">
+        <h1>You got the travel plans, we got the travel vans.</h1>
+        <p>
+          Add adventure to your life by joining the #vanlife movement. Rent the
+          perfect van to make your perfect road trip.
+        </p>
 
-      <Link to="vans">Find your van</Link>
+        <Link to="vans">Find your van</Link>
+      </div>
     </div>
   );
 }
