d1510a87ab2530348b50ce0704d63aee3458d21c
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-06-27 15:52:55 +0200

    NavLink

--- van-life/src/components/Header.jsx
+++ van-life/src/components/Header.jsx
@@ -1,16 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <header>
-      <Link className="site-logo" to="/">
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? "my-link" : null)}
+      >
         #VanLife
-      </Link>
+      </NavLink>
       <nav>
-        <Link to="/host">Host</Link>
-        <Link to="/about">About</Link>
-        <Link to="/vans">Vans</Link>
+        <NavLink
+          to="/host"
+          className={({ isActive }) => (isActive ? "my-link" : null)}
+        >
+          Host
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? "my-link" : null)}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/vans"
+          className={({ isActive }) => (isActive ? "my-link" : null)}
+        >
+          Vans
+        </NavLink>
       </nav>
     </header>
   );
