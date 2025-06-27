9113b9a89d71405ef0af3fa9ac8295fafb16adaa
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-06-27 11:34:47 +0200

    Update App.jsx

--- van-life/src/App.jsx
+++ van-life/src/App.jsx
@@ -18,15 +18,15 @@ export default function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VansDetail />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VansDetail />} />
         </Route>
 
-        <Route element={<HostLayout />}>
-          <Route path="/host" element={<Dashboard />} />
-          <Route path="/host/income" element={<Income />} />
-          <Route path="/host/reviews" element={<Reviews />} />
+        <Route path="host" element={<HostLayout />}>
+          <Route path="host" element={<Dashboard />} />
+          <Route path="income" element={<Income />} />
+          <Route path="reviews" element={<Reviews />} />
         </Route>
       </Routes>
       <footer className="footer">
