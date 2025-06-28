0476adc4425e4a19998b12bcbda0f14a17f7599b
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-06-28 18:43:00 +0200

    index.css

--- van-life/src/components/Header.jsx
+++ van-life/src/components/Header.jsx
@@ -6,26 +6,26 @@ export default function Header() {
     <header>
       <NavLink
         to="/"
-        className={({ isActive }) => (isActive ? "my-link" : null)}
+        className={({ isActive }) => (isActive ? "active-link" : null)}
       >
         #VanLife
       </NavLink>
       <nav>
         <NavLink
           to="/host"
-          className={({ isActive }) => (isActive ? "my-link" : null)}
+          className={({ isActive }) => (isActive ? "active-link" : null)}
         >
           Host
         </NavLink>
         <NavLink
           to="/about"
-          className={({ isActive }) => (isActive ? "my-link" : null)}
+          className={({ isActive }) => (isActive ? "active-link" : null)}
         >
           About
         </NavLink>
         <NavLink
           to="/vans"
-          className={({ isActive }) => (isActive ? "my-link" : null)}
+          className={({ isActive }) => (isActive ? "active-link" : null)}
         >
           Vans
         </NavLink>
