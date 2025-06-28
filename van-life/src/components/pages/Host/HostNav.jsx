3b5e29ef5c009576014f4708c917702984a8c422
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-06-28 18:59:55 +0200

    Update HostNav.jsx

--- van-life/src/components/pages/Host/HostNav.jsx
+++ van-life/src/components/pages/Host/HostNav.jsx
@@ -1,13 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "../styles/HostNav.css";
 
 export default function HostNav() {
+  const activeStyle = {
+    color: "#f0f2bd",
+    fontWeight: "bold",
+    textDecoration: "underline",
+  };
+
   return (
     <nav className="host-nav">
-      <Link to="/host">Dashboard</Link>
-      <Link to="/host/income">Income</Link>
-      <Link to="/host/reviews">Reviews</Link>
+      <NavLink
+        to="/host"
+        style={({ isActive }) => (isActive ? activeStyle : null)}
+      >
+        Dashboard
+      </NavLink>
+      <NavLink
+        to="/host/income"
+        style={({ isActive }) => (isActive ? activeStyle : null)}
+      >
+        Income
+      </NavLink>
+      <NavLink
+        to="/host/reviews"
+        style={({ isActive }) => (isActive ? activeStyle : null)}
+      >
+        Reviews
+      </NavLink>
     </nav>
   );
 }
