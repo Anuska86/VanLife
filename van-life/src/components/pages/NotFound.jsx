3855cb2691aabe6a0f2974024481c4d075dc2d1a
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-07-04 11:25:24 +0200

    Not Found

--- van-life/src/components/pages/NotFound.jsx
+++ van-life/src/components/pages/NotFound.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import "./styles/NotFound.css";
 import { Link } from "react-router-dom";
-import funnyVan from "../../images/funnyVan.png";
+import worriedVan from "../../images/worriedVan.png";
 
 export default function NotFound() {
   return (
     <div className="not-found-container">
-      <h1>404. NOT FOUND</h1>
-      <h1>Something's wrong here...</h1>
-      <h3>Sorry, we can't finde the page you're looking for.</h3>
-      <Link className="nf-return-button" to="/">
-        Return Home
-      </Link>
+      <div className="speech-bubble">
+        <h1>404. NOT FOUND</h1>
+        <h1>Something's wrong here...</h1>
+        <h3>Sorry, we can't finde the page you're looking for.</h3>
+        <Link className="nf-return-button" to="/">
+          Return Home
+        </Link>
+      </div>
+      <img src={worriedVan} alt="Worried Van" className="worried-van-img" />
     </div>
   );
 }
