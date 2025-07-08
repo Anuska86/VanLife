9b587bd340cb087fa516d059def6a45a22921802
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-07-08 15:11:24 +0200

    Dashboard

--- van-life/src/components/pages/Host/Dashboard.jsx
+++ van-life/src/components/pages/Host/Dashboard.jsx
@@ -1,11 +1,95 @@
 import React from "react";
-
+import "../../pages/styles/Dashboard.css";
+import { Link } from "react-router-dom";
+import { BsStarFill } from "react-icons/bs";
+import { getHostVans } from "../../../api";
 
 export default function Dashboard() {
+  const [vans, setVans] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    const fetchVans = async () => {
+      setLoading(true);
+
+      try {
+        const data = await getHostVans();
+        setVans(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchVans();
+  }, []);
+
+  function renderVanElements(vans) {
+    const hostVansElements = vans.map((van) => (
+      <div className="host-van-single" key={van.id}>
+        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+        <div className="host-van-info">
+          <h3>{van.name}</h3>
+          <p>{van.price}€/day</p>
+        </div>
+        <Link to={`vans/${van.id}`}>View</Link>
+      </div>
+    ));
+
+    return (
+      <div className="host-vans-list">
+        <section>{hostVansElements}</section>
+      </div>
+    );
+  }
+
+  if (loading)
+    return (
+      <h2 style={{ color: "brown" }} aria-live="polite">
+        Loading...
+      </h2>
+    );
+  if (error)
+    return (
+      <h2 style={{ color: "red" }} aria-live="assertive">
+        Ups! There was an error: {error.message}
+      </h2>
+    );
+
   return (
     <>
-      <h1>This is Host Dashboard</h1>
-     
+      <section className="host-dashboard-earnings">
+        <div className="info">
+          <h1>Welcome!</h1>
+          <p>
+            Income last <span>30 days</span>
+          </p>
+          <h2>$2,260</h2>
+        </div>
+        <Link to="income">Details</Link>
+      </section>
+      <section className="host-dashboard-reviews">
+        <h2>Review score</h2>
+
+        <BsStarFill className="star" />
+
+        <p>
+          <span>5.0</span>/5
+        </p>
+        <Link to="reviews">Details</Link>
+      </section>
+      <section className="host-dashboard-vans">
+        <div className="top">
+          <h2>Your listed vans</h2>
+          <Link to="vans">View all</Link>
+        </div>
+        {loading && !vans ? (
+          <h1>Loading...</h1>
+        ) : (
+          <>{renderVanElements(vans)}</>
+        )}
+      </section>
     </>
   );
 }
