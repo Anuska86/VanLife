aa84f2b5dd26900af9a15774359f44b9858f384e
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-07-09 14:57:47 +0200

    firestore

--- van-life/src/api.js
+++ van-life/src/api.js
@@ -6,6 +6,34 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(() => resolve(), ms));
 }
 
+//FIREBASE
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDxmy_PjSF6Y_jkFoptemwplDit8qbygc0",
+  authDomain: "vanlife-6c51a.firebaseapp.com",
+  projectId: "vanlife-6c51a",
+  storageBucket: "vanlife-6c51a.firebasestorage.app",
+  messagingSenderId: "662907236865",
+  appId: "1:662907236865:web:30cb7103809f0f7793c600",
+  measurementId: "G-Z3CVBCN8WT",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+//FUNCTIONS
+
 export async function getVans(id) {
   const url = id ? `/api/vans/${id}` : "/api/vans";
   const response = await fetch(url);
